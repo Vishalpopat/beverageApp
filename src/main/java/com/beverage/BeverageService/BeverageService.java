d565e2da2eae89942083deb37a4ce0c3ab70b11d
Vishal Popat <vishalpopat94@gmail.com>
2020-08-06 21:11:45 +0530

    Handle More Scenarios

--- src/main/java/com/beverage/BeverageService/BeverageService.java
+++ src/main/java/com/beverage/BeverageService/BeverageService.java
@@ -1,26 +1,49 @@
 package com.beverage.BeverageService;
 
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
 public class BeverageService {
+
+	private static Map<Items, List<Ingredients>> itemIngredientsMap = new HashMap<Items, List<Ingredients>>() {
+		{
+			put(Items.COFFEE, new ArrayList(Arrays.asList(Ingredients.MILK, Ingredients.SUGAR, Ingredients.WATER)));
+			put(Items.CHAI, new ArrayList(Arrays.asList(Ingredients.MILK, Ingredients.SUGAR, Ingredients.WATER)));
+			put(Items.BANANA_SMOOTHIE,	new ArrayList(Arrays.asList(Ingredients.MILK, Ingredients.SUGAR, Ingredients.WATER)));
+			put(Items.STRABERRY_SHAKE, new ArrayList(Arrays.asList(Ingredients.MILK, Ingredients.SUGAR, Ingredients.WATER)));
+			put(Items.MOJITO, new ArrayList(Arrays.asList(Ingredients.SUGAR, Ingredients.WATER, Ingredients.SODA, Ingredients.MINT)));
+		}
+	};
 
 	public double calculateTotalPrice(String input) {
 		double cost = 0;
 		try {
-		String[] items = input.replaceAll("[\"\\[\\]]", "").split(",");
-		
-		for(int i=0; i<items.length; i++) {
-			if(items[i].trim().startsWith("-")) {
-				cost -= Ingredients.fromName(items[i].trim().substring(1)).getValue();
-			}else {
-				cost += Items.fromName(items[i].trim()).getValue();
+			String[] items = input.replaceAll("[\"\\[\\]]", "").split(",");
+			Items item = null;
+			List<Ingredients> ingredients = null;
+			for (int i = 0; i < items.length; i++) {
+				
+				if (items[i].trim().startsWith("-")) {
+					Ingredients ingredient = Ingredients.valueOf(items[i].trim().substring(1).toUpperCase());
+					if (ingredients.contains(ingredient)) {
+						cost -= Ingredients.fromName(items[i].trim().substring(1)).getValue();
+						ingredients.remove(ingredient);
+					}
+				} else {
+					cost += Items.fromName(items[i].trim()).getValue();
+					item = Items.valueOf(items[i].trim().toUpperCase());
+					ingredients = new ArrayList<>(itemIngredientsMap.get(item));
+				}
 			}
-		}
-		}catch (Exception e) {
+		} catch (Exception e) {
 			e.printStackTrace();
 			throw e;
 		}
-		
+
 		return cost;
 	}
-	
 
 }
--- src/test/java/com/beverage/BeverageService/BeverageServiceTest.java
+++ src/test/java/com/beverage/BeverageService/BeverageServiceTest.java
@@ -20,6 +20,18 @@ public class BeverageServiceTest {
 		assertEquals(11.5, calculateTotalPrice, 0.0);
 	}
 	
+	@Test
+	public void duplicateIngredientTest() {
+		double calculateTotalPrice = beverageService.calculateTotalPrice("[\"Chai, -sugar, -sugar\", \"Chai\", \"Coffee, -milk, -milk\"]");
+		assertEquals(11.5, calculateTotalPrice, 0.0);
+	}
+	
+	@Test
+	public void ingredientNotUsedInItemTest() {
+		double calculateTotalPrice = beverageService.calculateTotalPrice("[\"Chai, -sugar\", \"Chai\", \"Coffee, -milk, -mint\"]");
+		assertEquals(11.5, calculateTotalPrice, 0.0);
+	}
+	
 	@Test(expected = IllegalArgumentException.class)
 	public void invalidItemNametest() {
 		beverageService.calculateTotalPrice("Random, -sugar");
@@ -29,5 +41,7 @@ public class BeverageServiceTest {
 	public void invalidIngredientNametest() {
 		beverageService.calculateTotalPrice("Chai, -random");
 	}
+	
+	
 
 }
