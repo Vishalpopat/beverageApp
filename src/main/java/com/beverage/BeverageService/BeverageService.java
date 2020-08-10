9d6b46d8c67835bdbdd5a556acb2f4b4daafa3d6
Vishal Popat <vishalpopat94@gmail.com>
2020-08-10 19:50:31 +0530

    Throw Exception on invalid input

--- src/main/java/com/beverage/BeverageService/BeverageService.java
+++ src/main/java/com/beverage/BeverageService/BeverageService.java
@@ -12,9 +12,12 @@ public class BeverageService {
 		{
 			put(Items.COFFEE, new ArrayList(Arrays.asList(Ingredients.MILK, Ingredients.SUGAR, Ingredients.WATER)));
 			put(Items.CHAI, new ArrayList(Arrays.asList(Ingredients.MILK, Ingredients.SUGAR, Ingredients.WATER)));
-			put(Items.BANANA_SMOOTHIE,	new ArrayList(Arrays.asList(Ingredients.MILK, Ingredients.SUGAR, Ingredients.WATER)));
-			put(Items.STRABERRY_SHAKE, new ArrayList(Arrays.asList(Ingredients.MILK, Ingredients.SUGAR, Ingredients.WATER)));
-			put(Items.MOJITO, new ArrayList(Arrays.asList(Ingredients.SUGAR, Ingredients.WATER, Ingredients.SODA, Ingredients.MINT)));
+			put(Items.BANANA_SMOOTHIE,
+					new ArrayList(Arrays.asList(Ingredients.MILK, Ingredients.SUGAR, Ingredients.WATER)));
+			put(Items.STRABERRY_SHAKE,
+					new ArrayList(Arrays.asList(Ingredients.MILK, Ingredients.SUGAR, Ingredients.WATER)));
+			put(Items.MOJITO, new ArrayList(
+					Arrays.asList(Ingredients.SUGAR, Ingredients.WATER, Ingredients.SODA, Ingredients.MINT)));
 		}
 	};
 
@@ -24,18 +27,26 @@ public class BeverageService {
 			String[] items = input.replaceAll("[\"\\[\\]]", "").split(",");
 			Items item = null;
 			List<Ingredients> ingredients = null;
+			List<Ingredients> ingredientNotNeeded = null;
 			for (int i = 0; i < items.length; i++) {
-				
+
 				if (items[i].trim().startsWith("-")) {
 					Ingredients ingredient = Ingredients.valueOf(items[i].trim().substring(1).toUpperCase());
 					if (ingredients.contains(ingredient)) {
-						cost -= Ingredients.fromName(items[i].trim().substring(1)).getValue();
-						ingredients.remove(ingredient);
+						if (ingredientNotNeeded.contains(ingredient)) {
+							throw new IllegalArgumentException("Given ingredient is already removed: " +ingredient.getName());
+						} else {
+							cost -= Ingredients.fromName(items[i].trim().substring(1)).getValue();
+							ingredientNotNeeded.add(ingredient);
+						}
+					} else {
+						throw new IllegalArgumentException("Given ingredient is not used in item: " +ingredient.getName()) ;
 					}
 				} else {
 					cost += Items.fromName(items[i].trim()).getValue();
 					item = Items.valueOf(items[i].trim().toUpperCase());
 					ingredients = new ArrayList<>(itemIngredientsMap.get(item));
+					ingredientNotNeeded = new ArrayList<>();
 				}
 			}
 		} catch (Exception e) {
--- src/test/java/com/beverage/BeverageService/BeverageServiceTest.java
+++ src/test/java/com/beverage/BeverageService/BeverageServiceTest.java
@@ -22,14 +22,20 @@ public class BeverageServiceTest {
 	
 	@Test
 	public void duplicateIngredientTest() {
-		double calculateTotalPrice = beverageService.calculateTotalPrice("[\"Chai, -sugar, -sugar\", \"Chai\", \"Coffee, -milk, -milk\"]");
-		assertEquals(11.5, calculateTotalPrice, 0.0);
+		try {
+			beverageService.calculateTotalPrice("[\"Chai, -sugar, -sugar\", \"Chai\", \"Coffee, -milk, -milk\"]");
+		} catch (IllegalArgumentException e) {
+			assertEquals("Given ingredient is already removed: sugar", e.getMessage());
+		}
 	}
 	
 	@Test
 	public void ingredientNotUsedInItemTest() {
-		double calculateTotalPrice = beverageService.calculateTotalPrice("[\"Chai, -sugar\", \"Chai\", \"Coffee, -milk, -mint\"]");
-		assertEquals(11.5, calculateTotalPrice, 0.0);
+		try {
+			beverageService.calculateTotalPrice("[\"Chai, -sugar\", \"Chai\", \"Coffee, -milk, -mint\"]");
+		} catch (IllegalArgumentException e) {
+			assertEquals("Given ingredient is not used in item: mint", e.getMessage());
+		}
 	}
 	
 	@Test(expected = IllegalArgumentException.class)
